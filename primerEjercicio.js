7133ed791bfe642c8c36373f0fc68a1068fbb811
Maidevs <socialmedia.by.by.mai@gmail.com>
2022-12-03 20:53:58 +0100

    ejercicios finalizados

---
+++ primerEjercicio.js
@@ -0,0 +1,8 @@
+var myFavoriteHero = "Hulk";
+var x = 50;
+var h = 5;
+var y = 10;
+var z = h + y;
+
+console.log("que vale z?", z);
+
---
+++ segundoEjercicio.js
@@ -0,0 +1,12 @@
+console.log(10*5);
+console.log(10/2);
+console.log(15%9);
+
+var y = 10
+var z = 5
+var x = y + z
+console.log("Cual es el valor de x?", x);
+
+
+console.log("Que vale ahora x?", y*z);
+
